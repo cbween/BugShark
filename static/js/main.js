289fbdce3e3a10e3f49b009dd8aa7c8c8e4aa695
Sasha Chedygov <sasha1rus@gmail.com>
2012-11-17 10:37:03 -0800

    Pushed JavaScript stuff

---
+++ static/js/main.js
@@ -0,0 +1,38 @@
+var BugShark = BugShark || {}
+
+
+/*
+ * Utility functions
+ */
+BugShark.utils = {
+    renderTemplate: function(id, data) {
+        var template = $('#template-' + id).text()
+        return Mustache.render(template, data)
+    },
+
+    /*
+     * An empty function
+     */
+    empty: function empty() {}
+}
+
+/*
+ * Contains the Backbone.js models
+ */
+BugShark.models = {}
+
+
+BugShark.collections = {}
+
+
+BugShark.views = {}
+
+BugShark.views.ToolBar = Backbone.View.extend({
+    el: '#bugshark-toolbar',
+
+    events: {
+        //
+    }
+})
+
+BugShark.tools = new BugShark.views.ToolBar()
