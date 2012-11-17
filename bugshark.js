9762b17bf9c0a960167876d7d184fb74cecdc7de
Sasha Chedygov <sasha1rus@gmail.com>
2012-11-17 13:24:29 -0800

    Dependencies are super annoying

--- bugshark.js
+++ bugshark.js
@@ -1,56 +1,60 @@
-var BugShark = BugShark || {}
+(function() {
+    var BugShark = BugShark || {}
 
-
-/*
- * Utility functions
- */
-BugShark.utils = {
-    renderTemplate: function(id, data) {
-        var template = BugShark.templates[id]
-        return Mustache.render(template, data)
-    },
 
     /*
-     * An empty function
+     * Utility functions
      */
-    empty: function empty() {}
-}
+    BugShark.utils = {
+        renderTemplate: function(id, data) {
+            var template = BugShark.templates[id]
+            return Mustache.render(template, data)
+        },
 
-
-/*
- * Contains the Backbone.js models
- */
-BugShark.models = {}
-
-
-BugShark.collections = {}
-
-
-/*
- * Not ideal, but it'll have to do for now
- */
-BugShark.templates = {
-    toolbar:
-        '<div id="bugshark-toolbar">' +
-            '<div class="heading">Feedback</div>' +
-            '<div class="content">' +
-                '<textarea rows="4" cols="20"></textarea>' +
-            '</div>' +
-        '</div>'
-}
-
-
-BugShark.views = {}
-
-BugShark.views.ToolBar = Backbone.View.extend({
-    events: {
-        //
-    },
-
-    initialize: function Toolbar_initialize() {
-        var html = BugShark.utils.renderTemplate('toolbar')
-        $(document.body).append(html)
+        /*
+         * An empty function
+         */
+        empty: function empty() {}
     }
-})
 
-BugShark.tools = new BugShark.views.ToolBar()
+
+    /*
+     * Contains the Backbone.js models
+     */
+    BugShark.models = {}
+
+
+    BugShark.collections = {}
+
+
+    /*
+     * Not ideal, but it'll have to do for now
+     */
+    BugShark.templates = {
+        toolbar:
+            '<div id="bugshark-toolbar">' +
+                '<div class="heading">Feedback</div>' +
+                '<div class="content">' +
+                    '<textarea rows="4" cols="20"></textarea>' +
+                '</div>' +
+            '</div>'
+    }
+
+
+    BugShark.views = {}
+
+    BugShark.views.ToolBar = Backbone.View.extend({
+        events: {
+            //
+        },
+
+        initialize: function Toolbar_initialize() {
+            var html = BugShark.utils.renderTemplate('toolbar')
+            $(document.body).append(html)
+        }
+    })
+
+    BugShark.tools = new BugShark.views.ToolBar()
+
+    window.BugShark = BugShark
+})()
