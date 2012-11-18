3c8d00cac6083afb1e25ffe3ea4b25fab6e60241
ColbyAley <colby@aley.me>
2012-11-18 13:37:57 -0800

    Adds Google Analytics

--- application/views/templates/foot.php
+++ application/views/templates/foot.php
@@ -25,6 +25,19 @@
 
 })()
 </script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36424763-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 <footer>
 	<hr style="color: #888888;">
 		<p style="font-size:13px; color: #888888;">Copyright 2012, BugShark</p>
