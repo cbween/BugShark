b50ccf0968ab7a49ec10a2cf7f78578442943458
Chris Breen <cbreen@launchside.com>
2012-11-18 12:04:18 -0800

    interface work

--- application/views/sites/detail.php
+++ application/views/sites/detail.php
@@ -1,1 +1,28 @@
-<?php print_r($this->data); ?>
+<h1><?php echo $site->title; ?></h1>
+<small><?php echo $site->url; ?></small>
+<p><span class="bold">Nice!</span> now its time to install bugshark on your website. If you have a developer, now is the time to call them over; if not don't fear we will show you how.</p>
+<p>Please paste the following just before the <span class="bold"><?php echo htmlspecialchars("</body>"); ?></span> tag</p>
+<textarea id="snippet">
+<script type="text/javascript">
+(function() {
+    var paramStr = location.search.substr(1)
+    var paramArray = paramStr.split('&')
+    var params = {}
+    for (var i = 0; i < paramArray.length; i++) {
+        var keyVal = paramArray[i].split('=')
+        params[keyVal[0]] = keyVal[1]
+    }
+    if (params.bugshark == 'true') {
+        var prefix = document.location.protocol == 'https:' ? 'https://' : 'http://'
+        var dump = '<link rel="stylesheet" type="text/css" href="' + prefix + 'bugshark.com/bugshark.css"/>'
+        var scripts = ['static/js/vendor/underscore', 'static/js/vendor/jquery-1.8.2.min', 'static/js/vendor/mustache', 'static/js/vendor/backbone', 'static/js/vendor/jquery.Jcrop.min', 'static/js/vendor/html2canvas.min', 'static/js/vendor/jquery.plugin.html2canvas', 'bugshark']
+        for (var j = 0; j < scripts.length; j++) {
+            dump += '<script type="text/javascript" src="' + prefix + 'bugshark.com/' + scripts[j] + '.js"></scri' + 'pt>'
+        }
+        dump += '<script type="text/javascript">BugShark.track_id = "<?php echo $site->track_id; ?>"</scri' + 'pt>'
+        document.write(dump)
+    }
+})()
+</script>
+</textarea>
+<a href="<?php echo base_url(); ?>sitemanager">Back</a>
--- application/views/templates/global-header.php
+++ application/views/templates/global-header.php
@@ -4,7 +4,10 @@
     <meta charset="utf-8">
     <title>BugShark</title>
     <!-- Le styles -->
-    <link href="<?php echo base_url(); ?>static/css/bootstrap.css" rel="stylesheet">
+
+	<link href="<?php echo base_url(); ?>static/css/bootstrap.css" rel="stylesheet">
+	<link href="<?php echo base_url(); ?>static/css/vendors/jquery-ui-1.9.1.custom.min.css" rel="stylesheet">
+
     <style type="text/css">
       body {
         padding-top: 60px;
