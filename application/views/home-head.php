486a8e0325efcee12b640d23444b29f34880b0bd
ColbyAley <colby@aley.me>
2012-11-18 15:41:30 -0800

    Centers stuff

--- application/views/home-head.php
+++ application/views/home-head.php
@@ -8,7 +8,7 @@
     	</style>
     	<iframe src="http://player.vimeo.com/video/53805056" width="600" height="350" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
     	<div class="hero-unit">
-        <h1>Bug reporting made easy.</h1>
+        <h1 style="text-align:center;">Bug reporting made easy.</h1>
         <p>BugShark provides businesses with a feedback platform to connect with a network of professionals, also known as  Sharks, who improve your website testing process by having them find bugs and give you visual and clear feedback on your site.</p>
         <p>Sign up now for a subscription or to offer a bounty to attract Sharks to your site.</p>
         <p><b>Don’t let the WebBugs bite! </b></p>
