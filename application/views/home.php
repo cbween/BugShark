e717942b8d47223b5e619b9625cb43787436ba00
ColbyAley <colby@aley.me>
2012-11-18 13:30:45 -0800

    Updates copy

--- application/views/home.php
+++ application/views/home.php
@@ -4,27 +4,27 @@
         <br>
         <div class="span4">
           <h2>Simple feedback tools</h2>
-          <p>Bug Shark’s markup tools allow Sharks (experts) to visually communicate the bugs found on your website to your developers, UX team, and content editors. Interactive drawing & text tools save your team from decoding bug reports, sifting through countless feedback emails, and the frustration of re-creating reported bugs.</p>
+          <p>BugShark’s markup tools allow Sharks to visually communicate the bugs found on your website to your developers, UX team, and content editors. Interactive drawing and text tools, with a snapshot of the markup, save your team from decoding bug reports, sifting through countless feedback emails, and the frustration of re-creating reported bugs.</p>
         </div>
         <div class="span4">
-        <a name="more">
-          <h2>Crowdsource Quality Assurance</h2>
-          <p>Bugshark utilizes an crowdsourced team of experts to find bugs fast and provide accurate bug reports. In exchange, website owners offer a flat fee for a set period of time for the pool of Sharks to find a variety of bugs. Bug severity determines the payout to the individual or group of Sharks.</p>
+          <h2>Quality Assurance</h2>
+          <p>BugShark allows you to utilize a crowdsourced team of experts or enable your internal testing partners to find bugs fast and provide accurate bug reports. To incentivize the Sharks we allow website owners to offer a bounty for a set period of time for the pool of Sharks to find a variety of bugs. Bug severity determines the payout to the individual or group of Sharks.</p>
         </div>
         <div class="span4">
-        <a name="more">
           <h2>Fast And Customizable</h2>
-          <p>We allow you to choose the length of time you will allow feedback, as well as choose your own bounty to set for interested sharks.</p>
+          <p>We allow you to choose the length of time you will allow feedback, as well as choose your amount of bounty.</p>
         </div>
         <div class="span4">
-        <a name="more">
           <h2>User Info Reports</h2>
           <p>Easily see user info such as browser, version, screen size, and operating system. Fix your bugs faster and easier than ever with reports from BugShark.</p>
         </div>
         <div class="span4">
-        <a name="more">
           <h2>Cross Browser Capability</h2>
-          <p>Not single browser contains all your bugs, so why should BugShark be limited? BugShark works in all major browsers.</p>
+          <p>No single browser contains all your bugs, so why should BugShark be limited? BugShark works in all major browsers.</p>
+        </div>
+        <div class="span4">
+          <h2>Sharks</h2>
+          <p>Our unique community of crowdsourced Sharks are able to build their reputation and earn a portion of the bounty. This keeps them incentivized to come back for more.</p>
         </div>
         <p><a href="#" style="text-align:center;" class="btn btn-primary">Go Back Up</a></p>
 
