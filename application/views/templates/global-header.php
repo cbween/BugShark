d6b9761c00b6798fb8e89774983ff2db0c2f6d73
ColbyAley <colby@aley.me>
2012-11-17 21:54:28 -0800

    Fixes CSS etc

--- application/views/templates/global-header.php
+++ application/views/templates/global-header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>BugShark</title>
     <!-- Le styles -->
-    <link href="bootstrap.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>static/css/bootstrap.css" rel="stylesheet">
     <style type="text/css">
       body {
         padding-top: 60px;
