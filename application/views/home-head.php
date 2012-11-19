cf3d92cece1849b52e52953016c33e96680d203f
ColbyAley <colby@aley.me>
2012-11-18 16:22:53 -0800

    Changes to homepage

--- application/views/home-head.php
+++ application/views/home-head.php
@@ -8,10 +8,10 @@
     	</style>
     	<div id="video-play"></div>
     	<div class="hero-unit">
-        <h1 style="text-align:center;">Bug reporting made easy.</h1>
+        <h2 style="text-align:center;">Get higher conversions through a bug free website.</h2>
         <p>BugShark provides businesses with a feedback platform to connect with a network of professionals, also known as  Sharks, who improve your website testing process by having them find bugs and give you visual and clear feedback on your site.</p>
         <p>Sign up now for a subscription or to offer a bounty to attract Sharks to your site.</p>
-        <p><b>Don’t let the WebBugs bite! </b></p>
+        <p style="text-align:center;"><b>Don’t let the WebBugs bite!</b></p>
         <div id="actions">
 	        <p><a href="#more" class="btn btn-large btn-primary">Learn more</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 	        <a href="user/register" class="btn btn-large btn-primary ">Sign Up</a></p>
